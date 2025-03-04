05e784b07e495eee55935254e3ac02d19ecf70a6
Marie COLLET <marie.collet2023@gmail.com>
2025-03-04 11:16:53 +0100

    Test présence bouton Ajouter au panier

--- cypress/e2e/smokeTests.cy.js
+++ cypress/e2e/smokeTests.cy.js
@@ -6,3 +6,27 @@ describe("Page de connexion", ()=> {
     cy.getBySel("login-submit").should("be.visible");
   });
 });
+
+describe("Vérification du bouton Ajouter au panier", () => {
+  it("Doit afficher le bouton Ajouter au panier sur une fiche produit après connexion", () => {
+    cy.request({
+      method: "POST",
+      url: `${Cypress.env('apiUrl')}/login`,
+      body: {
+          username: "test2@test.fr",
+          password: "testtest"
+      }
+  }).then((response) => {
+      expect(response.status).to.eq(200); 
+      const token = response.body.token;
+
+      cy.window().then((win) => {
+        win.localStorage.setItem("user", token)
+      });
+      cy.reload();
+
+      cy.visit("#/products/random");
+      cy.contains("Ajouter au panier").should("be.visible");
+    });
+  });
+});
