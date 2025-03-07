7041a80d1c2c77546bf57fbbff802dee3585ae66
Marie COLLET <marie.collet2023@gmail.com>
2025-03-07 10:35:05 +0100

    Test API liste produit

--- cypress/e2e/api.cy.js
+++ cypress/e2e/api.cy.js
@@ -1,13 +1,59 @@
-const apiOrders = `${Cypress.env("apiUrl")}/orders`;
+describe("le pannier", () => {
+  const apiOrders = `${Cypress.env("apiUrl")}/orders`;
 
-context("GET /orders", () => {
-  it("La requête doit me retourner un code erreur 401", () => {
+  context("GET /orders", () => {
+    it("La requête doit me retourner un code erreur 401", () => {
+      cy.request({
+        method: "GET",
+        url: apiOrders,
+        failOnStatusCode: false,
+      }).then((response) => {
+        expect(response.status).to.eq(401);
+      });
+    });
+  });
+
+
+  beforeEach(() => {
+    cy.request({
+      method: "POST",
+      url: `${Cypress.env("apiUrl")}/login`,
+      body: {
+        username: "test2@test.fr",
+        password: "testtest"
+      }
+    }).then((response) => {
+      expect(response.status).to.eq(200);
+      const token = response.body.token;
+
+      Cypress.env("authToken", token);
+    });
+  });
+  it("La requête doit me renvoyer la liste des produits qui sont dans le panier", () => {
     cy.request({
       method: "GET",
       url: apiOrders,
-      failOnStatusCode: false,
+      headers: {
+        Authorization: `Bearer ${Cypress.env("authToken")}`
+      }
     }).then((response) => {
-      expect(response.status).to.eq(401);
+      expect(response.status).to.eq(200);
+      expect(response.body).to.have.property("orderLines").that.is.an("array");
+
+      if (response.body.orderLines.length > 0) {
+        response.body.orderLines.forEach((line) => {
+          expect(line).to.have.property("id");
+          expect(line).to.have.property("quantity");
+          expect(line).to.have.property("product");
+
+          expect(line.product).to.have.property("id");
+          expect(line.product).to.have.property("name");
+          expect(line.product).to.have.property("price");
+        });
+      } else {
+        cy.log("Le panier est vide");
+      }
     });
   });
+
 });
