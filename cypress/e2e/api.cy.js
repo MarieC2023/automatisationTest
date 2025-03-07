b943a208c5e395023175ab6e8aac6fcddfc36f23
Marie COLLET <marie.collet2023@gmail.com>
2025-03-07 11:20:28 +0100

    Création du test fiche produit

--- cypress/e2e/api.cy.js
+++ cypress/e2e/api.cy.js
@@ -1,4 +1,20 @@
-describe("le pannier", () => {
+beforeEach(() => {
+  cy.request({
+    method: "POST",
+    url: `${Cypress.env("apiUrl")}/login`,
+    body: {
+      username: "test2@test.fr",
+      password: "testtest"
+    }
+  }).then((response) => {
+    expect(response.status).to.eq(200);
+    const token = response.body.token;
+
+    Cypress.env("authToken", token);
+  });
+});
+
+describe("Test sur le pannier", () => {
   const apiOrders = `${Cypress.env("apiUrl")}/orders`;
 
   context("GET /orders", () => {
@@ -13,22 +29,6 @@ describe("le pannier", () => {
     });
   });
 
-
-  beforeEach(() => {
-    cy.request({
-      method: "POST",
-      url: `${Cypress.env("apiUrl")}/login`,
-      body: {
-        username: "test2@test.fr",
-        password: "testtest"
-      }
-    }).then((response) => {
-      expect(response.status).to.eq(200);
-      const token = response.body.token;
-
-      Cypress.env("authToken", token);
-    });
-  });
   it("La requête doit me renvoyer la liste des produits qui sont dans le panier", () => {
     cy.request({
       method: "GET",
@@ -55,5 +55,34 @@ describe("le pannier", () => {
       }
     });
   });
+});
 
+describe("Test sur la fiche produit", () => {
+
+  const apiProducts = `${Cypress.env("apiUrl")}/products/random`;
+
+  it("La requête doit me renvoyer la liste de trois produits aléatoire", () => {
+    cy.request({
+      method: "GET",
+      url: apiProducts,
+      headers: {
+        Authorization: `Bearer ${Cypress.env("authToken")}`
+      }
+    }).then((response) => {
+      expect(response.status).to.eq(200);
+      expect(response.body).to.be.an("array");
+      expect(response.body.length).to.be.greaterThan(0);
+
+      expect(response.body[0]).to.have.property("id");
+      expect(response.body[0]).to.have.property("name");
+      expect(response.body[0]).to.have.property("availableStock");
+      expect(response.body[0]).to.have.property("skin");
+      expect(response.body[0]).to.have.property("ingredients");
+      expect(response.body[0]).to.have.property("description");
+      expect(response.body[0]).to.have.property("price");
+      expect(response.body[0]).to.have.property("picture");
+      expect(response.body[0]).to.have.property("varieties");
+      
+    });
+  });
 });
