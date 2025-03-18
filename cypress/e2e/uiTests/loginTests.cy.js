35b54418db1b08f30abf48ca4a92e5a9ab980799
Marie COLLET <marie.collet2023@gmail.com>
2025-03-18 11:19:14 +0100

    Ajout tests sur page Connexion

--- cypress/e2e/uiTests/loginTests.cy.js
+++ cypress/e2e/uiTests/loginTests.cy.js
@@ -2,20 +2,41 @@
 // 1 cliquez sur le bouton de connexion
 // 2 affichage de la page avec le formulaire
 // 3 entrez les info de connexion email + mdp
-// 4 être rediriger vers page accueil - affichage du bouton panier
+// 4 être rediriger vers page accueil 
+// 5 vérifier la présence du bouton panier
 
+describe('Test de connexion', () => {
+  it("Devrait se connecter et rediriger vers la page d'accueil avec le bouton Mon panier", () => {
+    
+    cy.visit('baseUrl');
 
-describe("Connexion de l'utilisateur", () => {
-    beforeEach(() => {
-        cy.clearCookies();
-        cy.clearLocalStorage();
-        cy.visit("baseUrl");
+    cy.intercept('POST', '/login').as('loginRequest'); 
+     
+    cy.getBySel("nav-link-login").click();
+      
+    cy.getBySel("login-input-username").type('test2@test.fr');
+    cy.getBySel("login-input-password").type('testtest');
+    cy.getBySel("login-submit").click();
+
+    
+    cy.wait('@loginRequest')
+      .then((interception) => {
+        expect(interception.request.body).to.deep.equal({
+          username: 'test2@test.fr', 
+          password: 'testtest'
+        });
+
+        expect(interception.response.body).to.have.property('token');
       });
 
-    it("Présence des champs de saisie et du bouton de connexion", () => {
-      
-      cy.getBySel("nav-link-login").click();
-    
+    const token = 'token';
+    cy.window().then((win) => {
+      win.localStorage.setItem("user", token);
     });
+
+    cy.url().should('eq', "http://localhost:8080/#/");
+   
+    cy.getBySel("nav-link-cart").should('be.visible');
+
   });
-  
+});
