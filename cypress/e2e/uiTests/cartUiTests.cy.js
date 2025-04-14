817de4f75ba3f4bea6787dea14d28c583688bb10
Marie COLLET <marie.collet2023@gmail.com>
2025-04-14 14:50:34 +0200

    tests UI panier

---
+++ cypress/e2e/uiTests/cartUiTests.cy.js
@@ -0,0 +1,115 @@
+import { selectors } from "../../support/selectors";
+import credentials from "../../fixtures/credentials.json";
+
+
+import { getRandomProduct } from "../../services/apiProducts";
+import { getCart } from "../../services/apiCart";
+
+describe("Ajout au panier - Stock décrémenté", () => {
+  let token;
+
+  beforeEach(() => {
+    
+    cy.visit(credentials.baseURL);
+    cy.intercept("POST", "/login").as("loginRequest");
+    cy.goToLoginPage();
+    cy.login(); 
+    cy.wait("@loginRequest").then((interception) => {
+      token = interception.response.body.token;
+      cy.window().then((win) => {
+        win.localStorage.setItem("user", token);
+      });
+    });
+  });
+
+  it.only("Produit ajouté au panier, stock déduit", () => {
+    getRandomProduct(token).then((response) => {
+      const product = response.body[0];
+      const productId = product.id;
+      const productName = product.name;
+      const productStock = product.availableStock;
+
+      cy.visit(`/#/products/${productId}`);
+
+      cy.get(selectors.productName).should("contain", productName);
+      cy.get(selectors.productStock).should("contain", productStock);
+
+      cy.get(selectors.quantityInput).clear().type("1");
+      cy.get(selectors.addToCartButton).click();
+
+      cy.url().should("include", "/#/cart");
+      cy.get(selectors.cartLineName).should("contain", productName);
+
+      cy.visit(`/#/products/${productId}`);
+
+      cy.get(selectors.productStock).should("contain", productStock - 1);
+    });
+  });
+
+  it("Empêche l'ajout au panier avec une quantité négative", () => {
+    getRandomProduct(token).then((response) => {
+      const product = response.body[0];
+      const productId = product.id;
+      const productName = product.name;
+      const productStock = product.availableStock;
+
+      cy.visit(`/#/products/${productId}`);
+      cy.get(selectors.productName).should("contain", productName);
+      cy.get(selectors.productStock).should("contain", productStock);
+
+      cy.get(selectors.quantityInput).clear().type("-1");
+      cy.get(selectors.quantityInput).should("have.class", "ng-invalid");
+      cy.get(selectors.quantityInput).should("have.value", "1");
+    });
+  });
+
+  it("Empêche l'ajout au panier avec une quantité supérieur à 20", () => {
+    getRandomProduct(token).then((response) => {
+      const product = response.body[0];
+      const productId = product.id;
+      const productName = product.name;
+      const productStock = product.availableStock;
+
+      cy.visit(`/#/products/${productId}`);
+      cy.get(selectors.productName).should("contain", productName);
+      cy.get(selectors.productStock).should("contain", productStock);
+
+      cy.get(selectors.quantityInput).clear().type("21");
+      cy.get(selectors.quantityInput).should("have.value", "20")
+    });
+  });
+
+  it("Produit ajouté au panier, présent dans le panier via l'API", () => {
+    getRandomProduct(token).then((response) => {
+      const product = response.body[0];
+      const productId = product.id;
+      const productName = product.name;
+      const productStock = product.availableStock;
+
+      cy.visit(`/#/products/${productId}`);
+
+      cy.get(selectors.productName).should("contain", productName);
+      cy.get(selectors.productStock).should("contain", productStock);
+
+      cy.get(selectors.quantityInput).clear().type("1");
+      cy.get(selectors.addToCartButton).click();
+
+      cy.url().should("include", "/#/cart");
+      cy.get(selectors.cartLineName).should("contain", productName);
+
+      getCart(token).then((cartResponse) => {
+        const orderLines = cartResponse.body.orderLines;
+  
+        expect(orderLines).to.have.length.greaterThan(0);
+        const addedProduct = orderLines.find(
+          (line) => line.product.id === productId
+        );
+  
+        expect(addedProduct).to.exist;
+        expect(addedProduct.product.name).to.equal(productName);
+        expect(addedProduct.quantity).to.equal(1);
+    });
+  });
+});
+});
+
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,66 +1,21 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
-
-
-
 import { selectors } from "./selectors";
 import credentials from "../fixtures/credentials.json";
 
-import './apiCommands/apiAuth';
-import './apiCommands/apiCart';
-import './apiCommands/apiProducts';
-import './apiCommands/apiReviews';
 
 Cypress.Commands.add("getBySel", (selector, ...args) => {
-  return cy.get(`[data-cy=${selector}]`, ...args)
-})
+  return cy.get(`[data-cy=${selector}]`, ...args);
+});
 
-Cypress.Commands.add('goToLoginPage', () => {
+
+Cypress.Commands.add("goToLoginPage", () => {
   cy.get(selectors.loginButton).click();
 });
 
-Cypress.Commands.add('login', () => {
+
+Cypress.Commands.add("login", () => {
+  cy.visit("");
+  cy.goToLoginPage();
   cy.get(selectors.usernameField).type(credentials.user.username);
   cy.get(selectors.passwordField).type(credentials.user.password);
   cy.get(selectors.submitButton).click();
-});
-
-Cypress.Commands.add("loginSession", () => {
-  cy.session([credentials.user.username, credentials.user.password], () => {
-    cy.visit(credentials.baseURL);
-    cy.goToLoginPage();
-    cy.login();
-
-    cy.wait('@loginRequest').then((interception) => {
-      const token = interception.response.body.token;
-      if (token) {
-        cy.window().then((win) => {
-          win.localStorage.setItem("user", token);
-        });
-      }
-    });
-  });
 });
